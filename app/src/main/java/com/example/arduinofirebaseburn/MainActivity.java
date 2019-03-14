cfed78b02068810fef5a7988c70f7afa03fff1dd
ozgurkalkan <ahmetozgurkalkan@gmail.com>
2019-03-14 14:00:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/arduinofirebaseburn/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.arduinofirebaseburn;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference ledstatus1 = myRef.child("led1").child("status");
+
+
+
+    Button b1;
+    Button b2;
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+
+        textView1 = (TextView)findViewById(R.id.textView);
+        //updateText(ledstatus1,textView1);
+
+        ledstatus1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+                textView1.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("ON");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("OFF");
+            }
+        });
+
+
+
+    }
+}
